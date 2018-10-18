12b6274b4368ad03af7ada6b7a03b4ccea6c1014
FredaFei <964959033@qq.com>
2018-10-18 16:38:59 +0800

    初步实现 toast

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import ButtonGroup from './buttonGroup'
 import Input from './input'
 import GCol from './gCol'
 import GRow from './gRow'
+import Toast from './toast'
+import plugin from './plugin'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
@@ -12,6 +14,8 @@ Vue.component('g-button-group', ButtonGroup)
 Vue.component('g-input', Input)
 Vue.component('g-col', GCol)
 Vue.component('g-row', GRow)
+Vue.component('g-toast', Toast)
+Vue.use(plugin)
 
 new Vue({
     el: '#app',
@@ -21,6 +25,15 @@ new Vue({
             value: '张三',
             message: '名字长度需大于2',
             value2: 'ha'
+        }
+    },
+    mounted() {
+        console.log(123)
+        this.showTaost()
+    },
+    methods: {
+        showTaost(){
+            this.$toast('i am a toast!')
         }
     }
 })
---
+++ src/toast.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="toast">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'gTaost'
+}
+</script>
+
+<style lang="scss" scoped>
+.toast {
+    display: flex;
+    padding: 8px 16px;
+    border-radius: 6px;
+    color: #fff;
+    font-size: 14px;
+    background: rgba(0, 0, 0, .8);
+    box-shadow: 0 1px 3px rgba(0,0,0,.6);
+}
+</style>
