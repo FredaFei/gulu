0355f6263000b30660de47125699ab0ef89884dd
FredaFei <964959033@qq.com>
2018-10-24 14:31:09 +0800

    当容器有 overflow hidden 的bug

--- src/popover.vue
+++ src/popover.vue
@@ -1,45 +1,57 @@
 <template>
     <div class="popover-wrapper" @click.stop="onClose">
-        <div class="popover-content" ref="popoverContent" v-if="isVisible">
+        <div class="popover-content" ref="popoverContent" @click.stop v-if="isVisible">
             <slot name="popover"></slot>
         </div>
-        <slot></slot>
+        <div class="trigger" ref="trigger">
+            <slot></slot>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     name: 'guluPopover',
-    data(){
+    data() {
         return {
             isVisible: false
         }
     },
     methods: {
-        onClose(){
+        onClose() {
             console.log('btn click')
             this.isVisible = !this.isVisible
-            if(this.isVisible){
-                this.$nextTick(()=>{
+            if (this.isVisible) {
+                this.$nextTick(() => {
                     document.body.appendChild(this.$refs.popoverContent)
+                    let {left,top,width, height} = this.$refs.trigger.getBoundingClientRect()
+                    console.log(left, top, width, height)
+                    this.$refs.popoverContent.style.left = left + window.scrollX + 'px'
+                    this.$refs.popoverContent.style.top = top + window.scrollY + 'px'
                 })
             }
-            const close = ()=>{
+            const close = () => {
                 console.log('body click')
                 this.isVisible = false
-                document.documentElement.removeEventListener('click',close)
+                document.documentElement.removeEventListener('click', close)
             }
-            document.documentElement.addEventListener('click',close)
+            document.documentElement.addEventListener('click', close)
         }
     }
 }
 </script>
 
 <style lang="scss" scoped>
-.popover-wrapper{
-
+.popover-wrapper {
+    display: inline-block;
+    vertical-align: top;
+    position: relative;
+    border: 1px solid red;
 }
-.popover-content{
-    
+.popover-content {
+    position: absolute;
+    // bottom: 100%;
+    transform: translateY(-100%);
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
 }
 </style>
