78c51d41b84fe9055d22226b306c28898aeab425
fei-han <964959033@qq.com>
2018-10-28 22:31:33 +0800

    重构 callapse

--- src/callapse.vue
+++ src/callapse.vue
@@ -28,24 +28,32 @@ export default {
         }
     },
     mounted() {
+        // 默认选中状态
         this.eventBus.$emit('update:select', this.selected)
-        this.eventBus.$on('update:addSelected', name => {
-            let selectedCopy = JSON.parse(JSON.stringify(this.selected))
-            if (this.single) {
-                selectedCopy = [name]
-            } else {
-                selectedCopy.push(name)
-            }
-            this.eventBus.$emit('update:selected', selectedCopy)
-            this.$emit('update:selected', selectedCopy)
-        })
-        this.eventBus.$on('update:removeSelected', name => {
-            let selectedCopy = JSON.parse(JSON.stringify(this.selected))
-            let index = selectedCopy.indexOf(name)
-            selectedCopy.splice(index, 1)
-            this.eventBus.$emit('update:selected', selectedCopy)
-            this.$emit('update:selected', selectedCopy)
-        })
+        let selectedCopy = JSON.parse(JSON.stringify(this.selected))
+        this.onAddSelected(selectedCopy)
+        this.onRemoveSelected(selectedCopy)
+    },
+    methods: {
+        onAddSelected(selectedCopy) {
+            this.eventBus.$on('update:addSelected', name => {
+                if (this.single) {
+                    selectedCopy = [name]
+                } else {
+                    selectedCopy.push(name)
+                }
+                this.eventBus.$emit('update:selected', selectedCopy)
+                this.$emit('update:selected', selectedCopy)
+            })
+        },
+        onRemoveSelected(selectedCopy) {
+            this.eventBus.$on('update:removeSelected', name => {
+                let index = selectedCopy.indexOf(name)
+                selectedCopy.splice(index, 1)
+                this.eventBus.$emit('update:selected', selectedCopy)
+                this.$emit('update:selected', selectedCopy)
+            })
+        }
     }
 }
 </script>
