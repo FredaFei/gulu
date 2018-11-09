1b58fa889d1b40fe8dd1c35981ef9aeaba6f3e7b
FredaFei <964959033@qq.com>
2018-11-09 11:57:45 +0800

    更新 button组件的测试用例， 还有两个待解决

--- tests/unit/button.spec.js
+++ tests/unit/button.spec.js
@@ -3,80 +3,76 @@ import sinon from 'sinon'
 import sinonChai from 'sinon-chai'
 chai.use(sinonChai)
 
-import { shallowMount } from '@vue/test-utils'
+import { mount,shallowMount } from '@vue/test-utils'
 import Vue from 'vue'
-import Button from '../../src/button'
+import Button from '@/button'
 
 describe('Button', () => {
-  it('存在.', () => {
-    expect(Button).to.be.ok
-  })
-  describe('props', () => {
-    const Constructor = Vue.extend(Button)
-    let vm
-    afterEach(() => {
-      vm.$destroy()
+    it('存在.', () => {
+        expect(Button).to.be.ok
     })
-    xit('可以设置icon.', () => {
-      vm = new Constructor({
-        propsData: {
-          icon: 'settings'
-        }
-      }).$mount()
-      const useElement = vm.$el.querySelector('use')
-      expect(useElement.getAttribute('xlink:href')).to.equal('#i-settings')
+    describe('props', () => {
+        it('可以设置icon.', () => {
+            const wrapper = mount(Button, {
+                propsData: {
+                    icon: 'settings'
+                }
+            })
+            let el = wrapper.find('use')
+            expect(el.attributes('href')).to.equal('#i-settings')
+        })
+        it('可以设置loading.', () => {
+            const wrapper = mount(Button, {
+                propsData: {
+                    loading: true
+                }
+            })
+            const { vm } = wrapper
+            let el = wrapper.find('use')
+            expect(el.attributes('href')).to.equal('#i-loading')
+        })
+        xit('icon 默认的 order 是 1', () => {
+            const wrapper = shallowMount(Button, {
+                attachToDocument: true,
+                propsData: {
+                    icon: 'settings',
+                    iconPosition: 'left'
+                }
+            })
+            const { vm } = wrapper
+            let el = vm.$el.querySelector('svg')
+            console.log(getComputedStyle(el))
+            console.log(vm.$el)
+            expect(wrapper.contains('.icon-left')).to.eq(true)
+            expect(getComputedStyle(el).order).to.eq('1')
+        })
+        xit('设置 iconPosition 可以改变 order', () => {
+            const wrapper = mount(Button, {
+                attachToDocument: true,
+                propsData: {
+                    icon: 'settings',
+                    iconPosition: 'right'
+                }
+            })
+            const { vm } = wrapper
+            const el = vm.$el.querySelector('svg')
+            expect(wrapper.contains('.icon-right')).to.eq(true)
+            expect(getComputedStyle(el).order).to.eq('2')
+        })
     })
-    xit('可以设置loading.', () => {
-      vm = new Constructor({
-        propsData: {
-          icon: 'settings',
-          loading: true
-        }
-      }).$mount()
-      const useElements = vm.$el.querySelectorAll('use')
-      expect(useElements.length).to.equal(1)
-      expect(useElements[0].getAttribute('xlink:href')).to.equal('#i-loading')
+    describe('事件', () => {
+        it('点击 button 触发 click 事件', () => {
+            const wrapper = mount(Button, {
+                propsData: {
+                    icon: 'settings'
+                }
+            })
+            const { vm } = wrapper
+            const callback = sinon.fake()
+            vm.$on('click', callback)
+            vm.$el.click()
+            // wrapper.trigger('click')
+            expect(callback).to.have.been.called
+        })
     })
-    xit('icon 默认的 order 是 1', () => {
-      const div = document.createElement('div')
-      document.body.appendChild(div)
-      vm = new Constructor({
-        propsData: {
-          icon: 'settings'
-        }
-      }).$mount(div)
-      const icon = vm.$el.querySelector('svg')
-      expect(getComputedStyle(icon).order).to.eq('1')
-      vm.$el.remove()
-    })
-    xit('设置 iconPosition 可以改变 order', () => {
-      const div = document.createElement('div')
-      document.body.appendChild(div)
-      vm = new Constructor({
-        propsData: {
-          icon: 'settings',
-          iconPosition: 'right'
-        }
-      }).$mount(div)
-      const icon = vm.$el.querySelector('svg')
-      expect(getComputedStyle(icon).order).to.eq('2')
-      vm.$el.remove()
-    })
-  })
-  describe('事件', () => {
-    const Constructor = Vue.extend(Button)
-    let vm
-    it('点击 button 触发 click 事件', () => {
-      vm = new Constructor({
-        propsData: {
-          icon: 'settings'
-        }
-      }).$mount()
-
-      const callback = sinon.fake()
-      vm.$on('click', callback)
-      vm.$el.click()
-      expect(callback).to.have.been.called
-    })
-  })
 })
