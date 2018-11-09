2ebb7bb1eaa24d436800b9e9d111b9ee29da8c1f
FredaFei <964959033@qq.com>
2018-11-09 16:01:39 +0800

    完成 row 组件 测试用例的改写

---
+++ tests/unit/row.spec.js
@@ -0,0 +1,47 @@
+import chai, { expect } from 'chai'
+import { mount } from '@vue/test-utils'
+import Vue from 'vue'
+
+import GRow from '@/gRow'
+import GCol from '@/gCol'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('GRow', () => {
+    it('存在.', () => {
+        expect(GRow).to.be.ok
+    })
+    describe('props', () => {
+        it('可以设置gutter.', () => {
+            Vue.component('g-col', GCol)
+            Vue.component('g-row', GRow)
+            const wrapper = mount(GRow, {
+                slots: {
+                    default: [`<g-row gutter="20">
+                    <g-col span="12">2</g-col>
+                    <g-col span="6">6</g-col>
+                    <g-col span="6">4</g-col>
+                </g-row>`]
+                },
+                attachToDocument: true,
+                propsData: {
+                    gutter: 'right'
+                }
+            })
+            const { vm } = wrapper
+            const rowEl = vm.$el.querySelectorAll('.row')[0]
+            expect(getComputedStyle(rowEl).marginLeft).to.eq('-10px')
+            expect(getComputedStyle(rowEl).marginRight).to.eq('-10px')
+        })
+        it('接受 align 属性', () => {
+            const wrapper = mount(GRow, {
+                attachToDocument: true,
+                propsData: {
+                    align: 'right'
+                }
+            })
+            expect(wrapper.classes('align-right')).to.eq(true)
+        })
+    })
+})
