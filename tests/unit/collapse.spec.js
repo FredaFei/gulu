8fee7a81566f12ad0ed2f720db72787b28781852
FredaFei <964959033@qq.com>
2018-11-27 16:24:57 +0800

    使用 test-utils 新增 collapseItem 组件 的测试用例

--- tests/unit/collapse.spec.js
+++ tests/unit/collapse.spec.js
@@ -8,34 +8,48 @@ import Vue from 'vue'
 import Collapse from '@/collapse'
 import CollapseItem from '@/collapseItem'
 
-Vue.component('g-collapse', Collapse)
 Vue.component('g-collapse-item', CollapseItem)
 
 describe('Collapse', () => {
-    it('存在.', () => {
-        expect(Collapse).to.be.ok
+  it('存在.', () => {
+    expect(Collapse).to.be.ok
+  })
+  it('selected 默认为第一个子元素.', (done) => {
+    const wrapper = mount(Collapse, {
+      slots: {
+        default: `
+        <g-collapse-item title="title_1" name="1"><span id="content_1">content_1</span></g-collapse-item>
+        <g-collapse-item title="title_2" name="2"><span id="content_2">content_2</span></g-collapse-item>
+        <g-collapse-item title="title_3" name="3"><span id="content_3">content_3</span></g-collapse-item>
+        <g-collapse-item title="title_4" name="4"><span id="content_4">content_4</span></g-collapse-item>
+        `
+      }
     })
-    // it('可以设置 single.', () => {
-    //     const wrapper = mount(Collapse)
-    //     // const wrapper = mount(Collapse,{})
-    //     // wrapper.setData({ selectedTab: ['1'] })
-    //     // wrapper.find('[data-name="2"]').trigger('click')
-    //     // expect(wrapper.find('#content_1')).to.not.exist
-    //     // expect(wrapper.find('#content_2')).to.exist
-    //     expect(1).to.eq(1)
-    // })
-    describe('props', () => {
-        xit('可以设置 single.', () => {
-            // const wrapper = mount(Collapse)
-            // console.log(wrapper)
-            const wrapper = mount(Collapse)
-            wrapper.setData({ foo: '123' })
-            // const wrapper = mount(Collapse,{})
-            // wrapper.setData({ selectedTab: ['1'] })
-            // wrapper.find('[data-name="2"]').trigger('click')
-            // expect(wrapper.find('#content_1')).to.not.exist
-            // expect(wrapper.find('#content_2')).to.exist
-            expect(1).to.eq(1)
-        })
+    setTimeout(() => {
+      expect(wrapper.find('#content_1').exists()).to.be.true
+      done()
     })
+  })
+  it('可以设置 single.', (done) => {
+    const wrapper = mount(Collapse, {
+      propsData: {
+        single: true,
+        selected: ['1']
+      },
+      slots: {
+        default: `
+        <g-collapse-item title="title_1" name="1"><span id="content_1">content_1</span></g-collapse-item>
+        <g-collapse-item title="title_2" name="2"><span id="content_2">content_2</span></g-collapse-item>
+        <g-collapse-item title="title_3" name="3"><span id="content_3">content_3</span></g-collapse-item>
+        <g-collapse-item title="title_4" name="4"><span id="content_4">content_4</span></g-collapse-item>
+        `
+      }
+    })
+    setTimeout(() => {
+      wrapper.find('[data-name="2"]').trigger('click')
+      expect(wrapper.contains('#content_1')).to.be.false
+      expect(wrapper.find('#content_2').exists()).to.be.true
+      done()
+    })
+  })
 })
---
+++ tests/unit/collapseItem.spec.js
@@ -0,0 +1,32 @@
+import chai, { expect } from 'chai'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+chai.use(sinonChai)
+import { mount } from '@vue/test-utils'
+
+import Vue from 'vue'
+import CollapseItem from '@/collapseItem'
+
+describe('CollapseItem', (done) => {
+  it('存在.', () => {
+    expect(CollapseItem).to.be.ok
+  })
+  it('可以设置 name.', () => {
+    const wrapper = mount(CollapseItem, {
+      propsData: {
+        name: 'test1',
+        title: 'title oo'
+      }
+    })
+    expect(wrapper.find('[data-name="test1"]').exists()).to.be.true
+  })
+  it('可以设置 title.', () => {
+    const wrapper = mount(CollapseItem, {
+      propsData: {
+        name: 'test1',
+        title: 'title oo'
+      }
+    })
+    expect(wrapper.find('.title').text()).to.be.eq('title oo')
+  })
+})
