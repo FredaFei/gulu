bc368b1eefe84c6ea95d11ac7048d0ff72e04b9d
fei-han <964959033@qq.com>
2018-12-15 17:36:58 +0800

    搭建pager基本原型

--- src/demo.vue
+++ src/demo.vue
@@ -1,86 +1,20 @@
 <template>
   <div id="demo">
-    <g-nav :selected.sync="selected" @update:selected="clickFn" vertical style="width:200px; margin-bottom:100px">
-      <g-nav-item name="music">
-        <div>nav 1</div>
-      </g-nav-item>
-      <g-sub-nav name="book">
-        <template slot="title">nav 2</template>
-        <g-nav-item name="faction">小说</g-nav-item>
-        <g-nav-item name="comic">动漫</g-nav-item>
-        <g-sub-nav name="classis">
-          <template slot="title">杂志期刊</template>
-          <g-nav-item name="technology">科技</g-nav-item>
-          <g-nav-item name="fashion">时尚</g-nav-item>
-          <g-sub-nav name="social">
-            <template slot="title">社会</template>
-            <g-nav-item name="11">社会 1</g-nav-item>
-            <g-nav-item name="22">社会 2</g-nav-item>
-            <g-nav-item name="33">社会 3</g-nav-item>
-          </g-sub-nav>
-        </g-sub-nav>
-      </g-sub-nav>
-      <g-nav-item name="sport">
-        <div>nav 3</div>
-      </g-nav-item>
-    </g-nav>
-    <g-nav :selected.sync="selected" style="width:300px;">
-      <g-nav-item name="music">
-        <div>nav 1</div>
-      </g-nav-item>
-      <g-sub-nav name="book">
-        <template slot="title">nav 2</template>
-        <g-nav-item name="faction">小说</g-nav-item>
-        <g-nav-item name="comic">动漫</g-nav-item>
-        <g-sub-nav name="classis">
-          <template slot="title">杂志期刊</template>
-          <g-nav-item name="technology">科技</g-nav-item>
-          <g-nav-item name="fashion">时尚</g-nav-item>
-          <g-sub-nav name="social">
-            <template slot="title">社会</template>
-            <g-nav-item name="11">社会 1</g-nav-item>
-            <g-nav-item name="22">社会 2</g-nav-item>
-            <g-nav-item name="33">社会 3</g-nav-item>
-          </g-sub-nav>
-        </g-sub-nav>
-      </g-sub-nav>
-      <g-nav-item name="sport">
-        <div>nav 3</div>
-      </g-nav-item>
-    </g-nav>
-    <p>test test test test test test test test test test</p>
+    <g-pager :current-page="2" :total-page="20"></g-pager>
   </div>
 </template>
 
 <script>
 import Vue from 'vue'
-import Nav from './nav/nav'
-import NavItem from './nav/navItem'
-import SubNav from './nav/subNav'
-Vue.component('g-nav', Nav)
-Vue.component('g-nav-item', NavItem)
-Vue.component('g-sub-nav', SubNav)
+import pager from './pager/pager'
+Vue.component('g-pager', Pager)
 
 export default {
   data() {
     return {
-      selected: 'sport'
+      
     }
   },
-  watch: {
-    selected(newValue) {
-      if (newValue === 'sport') {
-        alert(newValue)
-      }
-    }
-  },
-  methods: {
-    clickFn(name) {
-      if (name === 'music') {
-        alert(name)
-      }
-    }
-  }
 }
 
 </script>
---
+++ src/pager/pager.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="g-pager">
+    currentPage：{{currentPage}} totalPage：{{totalPage}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'guluPagination',
+  props:{
+    currentPage: {
+      type:Number,
+      required: true
+    },
+    totalPage: {
+      type:Number,
+      required: true
+    },
+    hideOnSinglePage: {
+      type:Boolean,
+      default: true
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
