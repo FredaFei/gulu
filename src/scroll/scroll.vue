fe699acee2df8c23c6c231f9955b3064acc6567f
FredaFei <964959033@qq.com>
2019-01-06 17:03:12 +0800

    创建scroll

--- src/scroll/scroll.vue
+++ src/scroll/scroll.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="g-scroll-wrapper">
-    <div class="g-scroll-content">
+  <div class="g-scroll-wrapper" ref="scrollParent">
+    <div class="g-scroll-content" ref="scrollChild">
       <slot></slot>
     </div>
   </div>
@@ -8,18 +8,40 @@
 
 <script>
 export default {
-  name: "gScroll"
+  name: "gScroll",
+  mounted() {
+    let parent = this.$refs.scrollParent;
+    let child = this.$refs.scrollChild;
+    let translateY = 0;
+    parent.addEventListener("wheel", e => {
+      // console.log(e);
+      if (e.deltaY > 0) {
+        translateY -= 10;
+        child.style.transform = `translateY(${translateY}px)`;
+      } else if (e.deltaY < 0) {
+        translateY += 10;
+        child.style.transform = `translateY(${translateY}px)`;
+      } else if ((e.deltaY = 0)) {
+        console.log("stay");
+      }
+    });
+    parent.addEventListener("touchmove", e => {
+      console.log(e);
+    });
+  }
 };
 </script>
 
 <style lang="scss" scoped>
 .g-scroll {
   &-wrapper {
-    border: 1px solid red;
+    overflow: hidden;
+    box-sizing: border-box;
+    border: 5px solid red;
   }
   &-content {
-    border: 1px solid blue;
-    overflow: auto;
+    transition: transform 0.2s;
+    border: 5px solid blue;
   }
 }
 </style>
