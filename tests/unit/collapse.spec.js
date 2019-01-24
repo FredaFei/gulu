592efe2ac3b1e026ca1b66c0e752fc93862373b5
FredaFei <964959033@qq.com>
2019-01-24 11:54:10 +0800

    新增 collapse组件的测试

--- tests/unit/collapse.spec.js
+++ tests/unit/collapse.spec.js
@@ -41,4 +41,77 @@ describe('Collapse', () => {
       done()
     })
   })
+  it('selected 默认为空数组.', done => {
+    const callback = sinon.fake()
+    const wrapper = mount(Collapse, {
+      propsData: {
+        single: true,
+        attachToDocument: true
+      },
+      slots: {
+        default: `
+        <g-collapse-item title="title_1" name="apple"><span id="content_1">content_1</span></g-collapse-item>
+        <g-collapse-item title="title_2" name="orange"><span id="content_2">content_2</span></g-collapse-item>
+        `
+      },
+      listeners: {
+        'update:selected': callback
+      }
+    })
+    setTimeout(() => {
+      expect(wrapper.find('.active').exists()).to.be.false
+      done()
+    })
+  })
+  it('single 默认为false.', done => {
+    const callback = sinon.fake()
+    const wrapper = mount(Collapse, {
+      propsData: {
+        selected: ['apple'],
+        attachToDocument: true
+      },
+      slots: {
+        default: `
+        <g-collapse-item title="title_1" name="apple"><span id="content_1">content_1</span></g-collapse-item>
+        <g-collapse-item title="title_2" name="orange"><span id="content_2">content_2</span></g-collapse-item>
+        <g-collapse-item title="title_2" name="banana"><span id="content_2">content_2</span></g-collapse-item>
+        `
+      },
+      listeners: {
+        'update:selected': callback
+      }
+    })
+    setTimeout(() => {
+      expect(wrapper.find('[data-name="apple"].active').exists()).to.be.true
+      wrapper.find('[data-name="banana"]').trigger('click')
+      expect(wrapper.find('[data-name="banana"].active').exists()).to.be.true
+      expect(callback).to.have.been.calledWith(['apple', 'banana'])
+      done()
+    })
+  })
+  it('关闭打开的collapseItem', done => {
+    const callback = sinon.fake()
+    const wrapper = mount(Collapse, {
+      propsData: {
+        selected: ['apple'],
+        attachToDocument: true
+      },
+      slots: {
+        default: `
+        <g-collapse-item title="title_1" name="apple"><span id="content_1">content_1</span></g-collapse-item>
+        <g-collapse-item title="title_2" name="orange"><span id="content_2">content_2</span></g-collapse-item>
+        <g-collapse-item title="title_2" name="banana"><span id="content_2">content_2</span></g-collapse-item>
+        `
+      },
+      listeners: {
+        'update:selected': callback
+      }
+    })
+    setTimeout(() => {
+      wrapper.find('[data-name="apple"]').trigger('click')
+      expect(callback).to.have.been.calledWith([])
+      expect(wrapper.find('[data-name="apple"].active').exists()).to.be.false
+      done()
+    })
+  })
 })
