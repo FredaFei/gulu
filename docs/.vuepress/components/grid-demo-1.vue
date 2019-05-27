1c2314cf328978d5cd06068ac3815d6ca9303d65
FredaFei <964959033@qq.com>
2019-05-27 09:59:07 +0800

    更新 grid 组件示例&文档

--- docs/.vuepress/components/grid-demo-1.vue
+++ docs/.vuepress/components/grid-demo-1.vue
@@ -4,85 +4,85 @@
       <h3>24格网格 预览</h3>
       <am-row class="demoRow">
         <am-col span="8">
-          <div class="demoCol">8</div>
+          <div class="demoCol">col-8</div>
         </am-col>
         <am-col span="8">
-          <div class="demoCol">8</div>
+          <div class="demoCol">col-8</div>
         </am-col>
         <am-col span="8">
-          <div class="demoCol">8</div>
+          <div class="demoCol">col-8</div>
         </am-col>
       </am-row>
       <am-row class="demoRow">
         <am-col span="6">
-          <div class="demoCol">6</div>
+          <div class="demoCol">col-6</div>
         </am-col>
         <am-col span="6">
-          <div class="demoCol">6</div>
+          <div class="demoCol">col-6</div>
         </am-col>
         <am-col span="6">
-          <div class="demoCol">6</div>
+          <div class="demoCol">col-6</div>
         </am-col>
         <am-col span="6">
-          <div class="demoCol">6</div>
+          <div class="demoCol">col-6</div>
         </am-col>
       </am-row>
       <am-row class="demoRow">
         <am-col span="4">
-          <div class="demoCol">4</div>
+          <div class="demoCol">col-4</div>
         </am-col>
         <am-col span="4">
-          <div class="demoCol">4</div>
+          <div class="demoCol">col-4</div>
         </am-col>
         <am-col span="4">
-          <div class="demoCol">4</div>
+          <div class="demoCol">col-4</div>
         </am-col>
         <am-col span="4">
-          <div class="demoCol">4</div>
+          <div class="demoCol">col-4</div>
         </am-col>
         <am-col span="4">
-          <div class="demoCol">4</div>
+          <div class="demoCol">col-4</div>
         </am-col>
         <am-col span="4">
-          <div class="demoCol">4</div>
+          <div class="demoCol">col-4</div>
         </am-col>
       </am-row>
       <am-row class="demoRow">
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
       </am-row>
     </div>
@@ -90,95 +90,111 @@
       <h3>设置gutter 预览</h3>
       <am-row class="demoRow" gutter="10">
         <am-col span="8">
-          <div class="demoCol">8</div>
+          <div class="demoCol">col-8</div>
         </am-col>
         <am-col span="8">
-          <div class="demoCol">8</div>
+          <div class="demoCol">col-8</div>
         </am-col>
         <am-col span="8">
-          <div class="demoCol">8</div>
+          <div class="demoCol">col-8</div>
         </am-col>
       </am-row>
       <am-row class="demoRow" gutter="10">
         <am-col span="6">
-          <div class="demoCol">6</div>
+          <div class="demoCol">col-6</div>
         </am-col>
         <am-col span="6">
-          <div class="demoCol">6</div>
+          <div class="demoCol">col-6</div>
         </am-col>
         <am-col span="6">
-          <div class="demoCol">6</div>
+          <div class="demoCol">col-6</div>
         </am-col>
         <am-col span="6">
-          <div class="demoCol">6</div>
+          <div class="demoCol">col-6</div>
         </am-col>
       </am-row>
       <am-row class="demoRow" gutter="10">
-        <am-col span="12"><div class="demoCol">12</div></am-col>
-        <am-col span="12"><div class="demoCol">12</div></am-col>
+        <am-col span="12">
+          <div class="demoCol">col-12</div>
+        </am-col>
+        <am-col span="12">
+          <div class="demoCol">col-12</div>
+        </am-col>
       </am-row>
       <am-row class="demoRow" gutter="10">
-        <am-col span="4"><div class="demoCol">4</div></am-col>
-        <am-col span="4"><div class="demoCol">4</div></am-col>
-        <am-col span="4"><div class="demoCol">4</div></am-col>
-        <am-col span="4"><div class="demoCol">4</div></am-col>
-        <am-col span="4"><div class="demoCol">4</div></am-col>
-        <am-col span="4"><div class="demoCol">4</div></am-col>
+        <am-col span="4">
+          <div class="demoCol">col-4</div>
+        </am-col>
+        <am-col span="4">
+          <div class="demoCol">col-4</div>
+        </am-col>
+        <am-col span="4">
+          <div class="demoCol">col-4</div>
+        </am-col>
+        <am-col span="4">
+          <div class="demoCol">col-4</div>
+        </am-col>
+        <am-col span="4">
+          <div class="demoCol">col-4</div>
+        </am-col>
+        <am-col span="4">
+          <div class="demoCol">col-4</div>
+        </am-col>
       </am-row>
     </div>
     <div style="paddinam-top: 16px;">
       <h3>设置offset 预览</h3>
       <am-row class="demoRow" gutter="10">
         <am-col span="8">
-          <div class="demoCol">8</div>
+          <div class="demoCol">col-8</div>
         </am-col>
         <am-col span="8" offset="8">
-          <div class="demoCol">8</div>
+          <div class="demoCol">col-8</div>
         </am-col>
       </am-row>
       <am-row class="demoRow" gutter="10">
         <am-col span="6" offset="6">
-          <div class="demoCol">6</div>
+          <div class="demoCol">col-6</div>
         </am-col>
         <am-col span="6" offset="6">
-          <div class="demoCol">6</div>
+          <div class="demoCol">col-6</div>
         </am-col>
       </am-row>
       <am-row class="demoRow" gutter="10">
         <am-col span="4">
-          <div class="demoCol">4</div>
+          <div class="demoCol">col-4</div>
         </am-col>
         <am-col span="4" offset="4">
-          <div class="demoCol">4</div>
+          <div class="demoCol">col-4</div>
         </am-col>
         <am-col span="4" offset="8">
-          <div class="demoCol">4</div>
+          <div class="demoCol">col-4</div>
         </am-col>
       </am-row>
       <am-row class="demoRow" gutter="10">
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2" offset="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2" offset="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2" offset="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2" offset="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
       </am-row>
     </div>
@@ -187,18 +203,20 @@
 </template>
 
 <script>
-import Col from '../../../src/grid/gCol'
-import Row from '../../../src/grid/gRow'
+import Col from "../../../src/grid/gCol";
+import Row from "../../../src/grid/gRow";
 export default {
   components: {
     AmCol: Col,
     AmRow: Row
   }
-}
+};
 </script>
 
 <style>
-.demoCol{
-  background: #ccc;
+.demoRow {
+  line-height: 50px;
+  text-align: center;
+  background: #e5e9f2;
 }
 </style>
--- docs/components/grid.md
+++ docs/components/grid.md
@@ -14,85 +14,85 @@ sidebarDepth: 2
       <h3>24格网格 预览</h3>
       <am-row class="demoRow">
         <am-col span="8">
-          <div class="demoCol">8</div>
+          <div class="demoCol">col-8</div>
         </am-col>
         <am-col span="8">
-          <div class="demoCol">8</div>
+          <div class="demoCol">col-8</div>
         </am-col>
         <am-col span="8">
-          <div class="demoCol">8</div>
+          <div class="demoCol">col-8</div>
         </am-col>
       </am-row>
       <am-row class="demoRow">
         <am-col span="6">
-          <div class="demoCol">6</div>
+          <div class="demoCol">col-6</div>
         </am-col>
         <am-col span="6">
-          <div class="demoCol">6</div>
+          <div class="demoCol">col-6</div>
         </am-col>
         <am-col span="6">
-          <div class="demoCol">6</div>
+          <div class="demoCol">col-6</div>
         </am-col>
         <am-col span="6">
-          <div class="demoCol">6</div>
+          <div class="demoCol">col-6</div>
         </am-col>
       </am-row>
       <am-row class="demoRow">
         <am-col span="4">
-          <div class="demoCol">4</div>
+          <div class="demoCol">col-4</div>
         </am-col>
         <am-col span="4">
-          <div class="demoCol">4</div>
+          <div class="demoCol">col-4</div>
         </am-col>
         <am-col span="4">
-          <div class="demoCol">4</div>
+          <div class="demoCol">col-4</div>
         </am-col>
         <am-col span="4">
-          <div class="demoCol">4</div>
+          <div class="demoCol">col-4</div>
         </am-col>
         <am-col span="4">
-          <div class="demoCol">4</div>
+          <div class="demoCol">col-4</div>
         </am-col>
         <am-col span="4">
-          <div class="demoCol">4</div>
+          <div class="demoCol">col-4</div>
         </am-col>
       </am-row>
       <am-row class="demoRow">
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
       </am-row>
     </div>
@@ -100,100 +100,117 @@ sidebarDepth: 2
       <h3>设置gutter 预览</h3>
       <am-row class="demoRow" gutter="10">
         <am-col span="8">
-          <div class="demoCol">8</div>
+          <div class="demoCol">col-8</div>
         </am-col>
         <am-col span="8">
-          <div class="demoCol">8</div>
+          <div class="demoCol">col-8</div>
         </am-col>
         <am-col span="8">
-          <div class="demoCol">8</div>
+          <div class="demoCol">col-8</div>
         </am-col>
       </am-row>
       <am-row class="demoRow" gutter="10">
         <am-col span="6">
-          <div class="demoCol">6</div>
+          <div class="demoCol">col-6</div>
         </am-col>
         <am-col span="6">
-          <div class="demoCol">6</div>
+          <div class="demoCol">col-6</div>
         </am-col>
         <am-col span="6">
-          <div class="demoCol">6</div>
+          <div class="demoCol">col-6</div>
         </am-col>
         <am-col span="6">
-          <div class="demoCol">6</div>
+          <div class="demoCol">col-6</div>
         </am-col>
       </am-row>
       <am-row class="demoRow" gutter="10">
-        <am-col span="12"><div class="demoCol">12</div></am-col>
-        <am-col span="12"><div class="demoCol">12</div></am-col>
+        <am-col span="12">
+          <div class="demoCol">col-12</div>
+        </am-col>
+        <am-col span="12">
+          <div class="demoCol">col-12</div>
+        </am-col>
       </am-row>
       <am-row class="demoRow" gutter="10">
-        <am-col span="4"><div class="demoCol">4</div></am-col>
-        <am-col span="4"><div class="demoCol">4</div></am-col>
-        <am-col span="4"><div class="demoCol">4</div></am-col>
-        <am-col span="4"><div class="demoCol">4</div></am-col>
-        <am-col span="4"><div class="demoCol">4</div></am-col>
-        <am-col span="4"><div class="demoCol">4</div></am-col>
+        <am-col span="4">
+          <div class="demoCol">col-4</div>
+        </am-col>
+        <am-col span="4">
+          <div class="demoCol">col-4</div>
+        </am-col>
+        <am-col span="4">
+          <div class="demoCol">col-4</div>
+        </am-col>
+        <am-col span="4">
+          <div class="demoCol">col-4</div>
+        </am-col>
+        <am-col span="4">
+          <div class="demoCol">col-4</div>
+        </am-col>
+        <am-col span="4">
+          <div class="demoCol">col-4</div>
+        </am-col>
       </am-row>
     </div>
     <div style="paddinam-top: 16px;">
       <h3>设置offset 预览</h3>
       <am-row class="demoRow" gutter="10">
         <am-col span="8">
-          <div class="demoCol">8</div>
+          <div class="demoCol">col-8</div>
         </am-col>
         <am-col span="8" offset="8">
-          <div class="demoCol">8</div>
+          <div class="demoCol">col-8</div>
         </am-col>
       </am-row>
       <am-row class="demoRow" gutter="10">
         <am-col span="6" offset="6">
-          <div class="demoCol">6</div>
+          <div class="demoCol">col-6</div>
         </am-col>
         <am-col span="6" offset="6">
-          <div class="demoCol">6</div>
+          <div class="demoCol">col-6</div>
         </am-col>
       </am-row>
       <am-row class="demoRow" gutter="10">
         <am-col span="4">
-          <div class="demoCol">4</div>
+          <div class="demoCol">col-4</div>
         </am-col>
         <am-col span="4" offset="4">
-          <div class="demoCol">4</div>
+          <div class="demoCol">col-4</div>
         </am-col>
         <am-col span="4" offset="8">
-          <div class="demoCol">4</div>
+          <div class="demoCol">col-4</div>
         </am-col>
       </am-row>
       <am-row class="demoRow" gutter="10">
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2" offset="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2" offset="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2" offset="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
         <am-col span="2" offset="2">
-          <div class="demoCol">2</div>
+          <div class="demoCol">col-2</div>
         </am-col>
       </am-row>
     </div>
   </div>
 </template>
+
 ```
 
 ### API
