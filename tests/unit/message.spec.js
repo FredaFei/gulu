964164bab24c4f3b91d5bb72f900c5b980fee451
FredaFei <964959033@qq.com>
2019-08-13 16:56:43 +0800

    更新 message组件和测试用例

--- tests/unit/message.spec.js
+++ tests/unit/message.spec.js
@@ -4,35 +4,75 @@ import sinonChai from 'sinon-chai'
 chai.use(sinonChai)
 
 import { mount } from '@vue/test-utils'
-import Vue from 'vue'
 import Message from '../../src/message/message'
 
 describe('Message', () => {
-  const Constructor = Vue.extend(Message)
-  let vm
-  // afterEach(() => {
-  //   vm.$destroy()
-  // })
-  it('存在.', () => {
+  it('Message 存在', () => {
     expect(Message).to.be.ok
   })
-  // it('可以设置 title.', () => {
-  //   vm = new Constructor({ propsData: { title: 'test'} }).$mount()
-  //   // todo
-  // })
-  // it('接受 enableHtml 属性', () => {
-  //   vm = new Constructor({
-  //     propsData: {
-  //       enableHtml: true
-  //     }
-  //   })
-  //   vm.$slots.default = ['<strong id="test">test strong</strong>']
-  //   vm.$mount()
-  //   let strong = vm.$el.querySelector('#test')
-  //   expect(strong).to.exist
-  // })
-  // it('可以设置 zIndex.', () => {
-  //   vm = new Constructor({ propsData: { zIndex: 99 } }).$mount()
-  //   expect(vm.$el.style.zIndex).to.eq('99')
-  // })
+  it('设置 title', () => {
+    const wrapper = mount(Message, {
+      propsData: {
+        title: 'test'
+      }
+    })
+    expect(wrapper.props().title).to.eq('test')
+  })
+  it('默认 title', () => {
+    const wrapper = mount(Message)
+    expect(wrapper.props().title).to.eq('标题')
+  })
+
+  it('设置 cancelButton', done => {
+    const callback = sinon.fake()
+    const wrapper = mount(Message, {
+      propsData: {
+        cancelButton: {
+          text: 'c',
+          callback
+        },
+        confirmButton: {}
+      },
+      slots: {
+        default: 'ha'
+      }
+    })
+    wrapper.setData({ visible: true })
+    setTimeout(() => {
+      const btn = wrapper.findAll('.am-message-btn')
+      expect(btn.at(0).exists()).to.eq(true)
+      expect(btn.at(0).text()).to.eq('c')
+      expect(btn.length).to.eq(1)
+      btn.at(0).trigger('click')
+      expect(callback).to.have.been.called
+      expect(wrapper.html()).to.eq(undefined)
+      done()
+    }, 20)
+  })
+  it('设置 confirmButton', done => {
+    const callback = sinon.fake()
+    const wrapper = mount(Message, {
+      propsData: {
+        confirmButton: {
+          text: 'a',
+          callback
+        },
+        cancelButton: {}
+      },
+      slots: {
+        default: 'ha'
+      }
+    })
+    wrapper.setData({ visible: true })
+    setTimeout(() => {
+      const btn = wrapper.findAll('.am-message-btn')
+      expect(btn.at(0).exists()).to.eq(true)
+      expect(btn.at(0).text()).to.eq('a')
+      expect(btn.length).to.eq(1)
+      btn.at(0).trigger('click')
+      expect(callback).to.have.been.called
+      expect(wrapper.html()).to.eq(undefined)
+      done()
+    }, 20)
+  })
 })
