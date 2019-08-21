def126c357bba04335a6e5c2ed94c05df3fffc3a
FredaFei <964959033@qq.com>
2019-08-21 17:12:09 +0800

    更新 checkbox 组件测试用例

--- tests/unit/checkbox.spec.js
+++ tests/unit/checkbox.spec.js
@@ -1,5 +1,9 @@
-import chai, { expect } from 'chai'
-import { mount } from '@vue/test-utils'
+import chai, {expect} from 'chai'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+
+chai.use(sinonChai)
+import {mount} from '@vue/test-utils'
 
 import AmCheckbox from '../../src/checkbox/index'
 
@@ -7,12 +11,140 @@ describe('AmCheckbox', () => {
   it('存在.', () => {
     expect(AmCheckbox).to.be.ok
   })
-  it('value', () => {
+  it('设置 disabled', () => {
+    const callback = sinon.fake()
     const wrapper = mount(AmCheckbox, {
       propsData: {
-        value: 12
+        model: true,
+        disabled: true
       }
     })
-    console.log(wrapper)
+    wrapper.trigger('click')
+    expect(callback).to.not.have.been.called
+  })
+  it('设置 name', () => {
+    const wrapper = mount(AmCheckbox, {
+      propsData: {
+        model: true,
+        name: 'ha'
+      }
+    })
+    expect(wrapper.props('name')).to.eq('ha')
+  })
+  it('设置 indeterminate', () => {
+    const wrapper = mount(AmCheckbox, {
+      propsData: {
+        model: false,
+        indeterminate: true
+      }
+    })
+    expect(wrapper.classes('am-checkbox-indeterminate')).to.eq(true)
+    expect(wrapper.props('indeterminate')).to.eq(true)
+  })
+  it('设置 v-model 为布尔', () => {
+    const wrapper = mount(AmCheckbox, {
+      propsData: {
+        model: true
+      }
+    })
+    expect(wrapper.props('model')).to.eq(true)
+  })
+  it('设置 v-model 为字符串, 默认不选中', (done) => {
+    const callback = sinon.fake()
+    const wrapper = mount(AmCheckbox, {
+      attachToDocument: true,
+      propsData: {
+        model: '',
+        value: 'a'
+      },
+      listeners: {
+        'change': callback
+      }
+    })
+    wrapper.trigger('click')
+    setTimeout(()=>{
+      expect(callback).to.have.been.calledWith('a')
+      done()
+    },10)
+  })
+  it('设置 v-model 为字符串, 默认选中', (done) => {
+    const callback = sinon.fake()
+    const wrapper = mount(AmCheckbox, {
+      attachToDocument: true,
+      propsData: {
+        model: 'a',
+        value: 'a'
+      },
+      listeners: {
+        'change': callback
+      }
+    })
+    wrapper.trigger('click')
+    setTimeout(()=>{
+      expect(callback).to.have.been.calledWith('')
+      done()
+    },10)
+  })
+  it('设置 v-model 为数组,默认为空数组', (done) => {
+    const callback = sinon.fake()
+    const wrapper = mount(AmCheckbox, {
+      propsData: {
+        model: [],
+        value: '1'
+      },
+      listeners: {
+        'change': callback
+      }
+    })
+    expect(wrapper.classes('am-checkbox-checked')).to.eq(false)
+    wrapper.trigger('click')
+    setTimeout(()=>{
+      expect(callback).to.have.been.calledWith(['1'])
+      done()
+    },20)
+  })
+  it('设置 v-model 为数组,默认选中一个', (done) => {
+    const callback = sinon.fake()
+    const wrapper = mount(AmCheckbox, {
+      propsData: {
+        model: ['1'],
+        value: '1'
+      },
+      listeners: {
+        'change': callback
+      }
+    })
+    expect(wrapper.classes('am-checkbox-checked')).to.eq(true)
+    wrapper.trigger('click')
+    setTimeout(()=>{
+      expect(callback).to.have.been.calledWith([])
+      done()
+    },20)
+  })
+  it('trueValue 默认为 true,falseValue 默认为 false, 设置model 为false', () => {
+    const callback = sinon.fake()
+    const wrapper = mount(AmCheckbox, {
+      propsData: {
+        model: false
+      },
+      listeners: {
+        'change': callback
+      }
+    })
+    wrapper.trigger('click')
+    expect(callback).to.have.been.calledWith(true)
+  })
+  it('trueValue 默认为 true,falseValue 默认为 false, 设置model 为false', () => {
+    const callback = sinon.fake()
+    const wrapper = mount(AmCheckbox, {
+      propsData: {
+        model: true
+      },
+      listeners: {
+        'change': callback
+      }
+    })
+    wrapper.trigger('click')
+    expect(callback).to.have.been.calledWith(false)
   })
 })
