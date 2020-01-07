9c21474e5362183beac1dfd903d3d025c36c5524
FredaFei <964959033@qq.com>
2020-01-07 15:49:28 +0800

    更新Tabs组件，添加lineWidthOrHeight

--- src/tabs/tabs.vue
+++ src/tabs/tabs.vue
@@ -5,78 +5,83 @@
 </template>
 
 <script>
-import Vue from "vue";
-export default {
-  name: "amTabs",
-  props: {
-    selected: {
-      type: [String, Number],
-      required: true
-    },
-    direction: {
-      type: String,
-      default: "horizontal",
-      validator(val) {
-        return ["horizontal", "vertical"].indexOf(val) > -1;
+  import Vue from "vue";
+
+  export default {
+    name: "amTabs",
+    props: {
+      selected: {
+        type: [String, Number],
+        required: true
+      },
+      direction: {
+        type: String,
+        default: "horizontal",
+        validator(val) {
+          return ["horizontal", "vertical"].indexOf(val) > -1;
+        }
+      },
+      lineWidthOrHeight: {
+        type: [String, Number]
       }
-    }
-  },
-  data() {
-    return {
-      eventBus: new Vue()
-    };
-  },
-  provide() {
-    return {
-      eventBus: this.eventBus,
-      direction: this.direction
-    };
-  },
-  computed: {
-    classes() {
-      return this.direction;
-    }
-  },
-  mounted() {
-    this.checkChildren();
-    this.selectedTabs();
-  },
-  methods: {
-    checkChildren() {
-      if (this.$children.length === 0) {
-        console &&
+    },
+    data() {
+      return {
+        eventBus: new Vue()
+      };
+    },
+    provide() {
+      return {
+        eventBus: this.eventBus,
+        direction: this.direction,
+        lineWidthOrHeight: this.lineWidthOrHeight
+      };
+    },
+    computed: {
+      classes() {
+        return this.direction;
+      }
+    },
+    mounted() {
+      this.checkChildren();
+      this.selectedTabs();
+    },
+    methods: {
+      checkChildren() {
+        if (this.$children.length === 0) {
+          console &&
           console.warn &&
           console.warn(
             "tabs的子组件应该是tabs-header和tabs-body,但是你没有写子组件"
           );
-      }
-    },
-    selectedTabs() {
-      this.$children.forEach(vm => {
-        if (vm.$options.name === "amTabsHeader") {
-          vm.$children.forEach(childVm => {
-            if (
-              childVm.$options.name === "amTabsItem" &&
-              childVm.name === this.selected
-            ) {
-              this.eventBus &&
-                this.eventBus.$emit("update:selected", this.selected, childVm);
-            }
-          });
         }
-      });
+      },
+      selectedTabs() {
+        this.$children.forEach(vm => {
+          if (vm.$options.name === "amTabsHeader") {
+            vm.$children.forEach(childVm => {
+              if (
+                childVm.$options.name === "amTabsItem" &&
+                childVm.name === this.selected
+              ) {
+                this.eventBus &&
+                this.eventBus.$emit("update:selected", this.selected, childVm);
+              }
+            });
+          }
+        });
+      }
     }
-  }
-};
+  };
 </script>
 
 <style lang="scss">
-.am-tabs-content {
-  position: relative;
-  &.vertical {
-    display: flex;
-    align-items: flex-start;
-    justify-content: flex-start;
+  .am-tabs-content {
+    position: relative;
+    &.vertical {
+      display: flex;
+      align-items: flex-start;
+      justify-content: flex-start;
+    }
   }
-}
 </style>
--- src/tabs/tabsHeader.vue
+++ src/tabs/tabsHeader.vue
@@ -10,71 +10,73 @@
 </template>
 
 <script>
-export default {
-  name: "amTabsHeader",
-  inject: ["eventBus", "direction"],
-  mounted() {
-    this.eventBus &&
+  export default {
+    name: "amTabsHeader",
+    inject: ["eventBus", "direction", "lineWidthOrHeight"],
+    mounted() {
+      this.eventBus &&
       this.eventBus.$on("update:selected", (name, vm) => {
         this.calculateLineStyle(vm);
       });
-  },
-  methods: {
-    calculateLineStyle(vm) {
-      // 当选中状态样式导致元素宽度变化时，会出现下划线的宽度与当前选中的tabItem宽度不一致
-      this.$nextTick(() => {
-        let {
-          left: left1,
-          top: top1
-        } = this.$refs.tabsHead.getBoundingClientRect();
-        let {
-          width,
-          left: left2,
-          height,
-          top: top2
-        } = vm.$el.getBoundingClientRect();
-        if (this.direction === "horizontal") {
-          this.$refs.line.style.width = `${width}px`;
-          this.$refs.line.style.left = `${left2 - left1}px`;
-        } else {
-          this.$refs.line.style.height = `${height}px`;
-          this.$refs.line.style.top = `${top2 - top1}px`;
-        }
-      });
+    },
+    methods: {
+      calculateLineStyle(vm) {
+        // 当选中状态样式导致元素宽度变化时，会出现下划线的宽度与当前选中的tabItem宽度不一致
+        this.$nextTick(() => {
+          let {
+            left: left1,
+            top: top1
+          } = this.$refs.tabsHead.getBoundingClientRect();
+          let {
+            width,
+            left: left2,
+            height,
+            top: top2
+          } = vm.$el.getBoundingClientRect();
+          const lineWidth = this.lineWidthOrHeight ? +this.lineWidthOrHeight : width
+          const lineHeight = this.lineWidthOrHeight ? +this.lineWidthOrHeight : height
+          if (this.direction === "horizontal") {
+            this.$refs.line.style.width = `${lineWidth}px`;
+            this.$refs.line.style.left = `${left2 - left1 + (width - lineWidth) / 2}px`;
+          } else {
+            this.$refs.line.style.height = `${lineHeight}px`;
+            this.$refs.line.style.top = `${top2 - top1 + (height - lineHeight) / 2}px`;
+          }
+        });
+      }
     }
-  }
-};
+  };
 </script>
 
 <style lang="scss" scoped>
-.am-tabs-header {
-  position: relative;
-  display: flex;
-  justify-content: flex-start;
-  flex-shrink: 0;
-  border-bottom: 1px solid #ddd;
-  > .line {
-    position: absolute;
-    bottom: -1px;
-    border-bottom: 2px solid #1890ff;
-    box-sizing: border-box;
-    transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
-  }
-  > .action-wrapper {
-    margin-left: auto;
+  .am-tabs-header {
+    position: relative;
     display: flex;
-    align-items: center;
-    justify-content: center;
-    padding: 0 1em;
-  }
-  &.vertical {
-    flex-direction: column;
-    border-right: 1px solid #ddd;
-    border-bottom: none;
+    justify-content: flex-start;
+    flex-shrink: 0;
+    border-bottom: 1px solid #ddd;
     > .line {
-      right: -1px;
-      border-right: 2px solid #1890ff;
+      position: absolute;
+      bottom: -1px;
+      border-bottom: 2px solid #1890ff;
+      box-sizing: border-box;
+      transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
+    }
+    > .action-wrapper {
+      margin-left: auto;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding: 0 1em;
+    }
+    &.vertical {
+      flex-direction: column;
+      border-right: 1px solid #ddd;
+      border-bottom: none;
+      > .line {
+        right: -1px;
+        border-right: 2px solid #1890ff;
+      }
     }
   }
-}
 </style>
