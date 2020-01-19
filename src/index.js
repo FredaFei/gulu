d04ca349dc133b08e061cf21f774218d1b70a2ab
FredaFei <964959033@qq.com>
2020-01-19 17:45:09 +0800

    导出 组件

--- src/index.js
+++ src/index.js
@@ -1,129 +1,134 @@
-import Button from './button/button.vue'
-import ButtonGroup from './button/buttonGroup'
-import ControllCount from './controllCount/index.vue'
-import Cascader from './cascader/cascader.vue'
-import CascaderItem from './cascader/cascaderItem.vue'
-import Collapse from './collapse/collapse.vue'
-import CollapseItem from './collapse/collapseItem.vue'
-import DatePicker from './datePicker/index.vue'
-import Col from './grid/gCol.vue'
-import Row from './grid/gRow.vue'
-import Input from './input/input.vue'
-import Checkbox from './checkbox/index.vue'
-import Radio from './radio/index.vue'
-import Select from './select/index.vue'
-import Option from './select/option.vue'
-import Table from './table/index.vue'
-import TableColumn from './table/tableColumn.vue'
-import Layout from './layout/layout.vue'
-import Header from './layout/header.vue'
-import Aside from './layout/aside.vue'
-import Content from './layout/content.vue'
-import Footer from './layout/footer.vue'
-import Nav from './nav/nav.vue'
-import NavItem from './nav/navItem.vue'
-import SubNav from './nav/subNav.vue'
-import Pager from './pager/pager.vue'
-import Popover from './popover/popover.vue'
-import Slides from './slides/slides.vue'
-import SlidesItem from './slides/slidesItem.vue'
-import Tabs from './tabs/tabs.vue'
-import TabsHeader from './tabs/tabsHeader.vue'
-import TabsItem from './tabs/tabsItem.vue'
-import TabsBody from './tabs/tabsBody.vue'
-import TabsPane from './tabs/tabsPane.vue'
-import Toast from './toast/index.js'
-import Uploader from './uploader/uploader.vue'
-import Icon from './icon.vue'
-import CollapseTransition from './collapseTransition.vue'
-import ClickOutside from './clickOutside.js'
+import AmButton from './button/button.vue'
+import AmButtonGroup from './button/buttonGroup'
+import AmControllCount from './controllCount/index.vue'
+import AmCascader from './cascader/cascader.vue'
+import AmCascaderItem from './cascader/cascaderItem.vue'
+import AmCollapse from './collapse/collapse.vue'
+import AmCollapseItem from './collapse/collapseItem.vue'
+import AmDatePicker from './datePicker/index.vue'
+import AmCol from './grid/gCol.vue'
+import AmRow from './grid/gRow.vue'
+import AmInput from './input/input.vue'
+import AmCheckbox from './checkbox/index.vue'
+import AmRadio from './radio/index.vue'
+import AmSelect from './select/index.vue'
+import AmOption from './select/option.vue'
+import AmTable from './table/index.vue'
+import AmTableColumn from './table/tableColumn.vue'
+import AmLayout from './layout/layout.vue'
+import AmHeader from './layout/header.vue'
+import AmAside from './layout/aside.vue'
+import AmContent from './layout/content.vue'
+import AmFooter from './layout/footer.vue'
+import AmNav from './nav/nav.vue'
+import AmNavItem from './nav/navItem.vue'
+import AmSubNav from './nav/subNav.vue'
+import AmPager from './pager/pager.vue'
+import AmPopover from './popover/popover.vue'
+import AmSlides from './slides/slides.vue'
+import AmSlidesItem from './slides/slidesItem.vue'
+import AmTabs from './tabs/tabs.vue'
+import AmTabsHeader from './tabs/tabsHeader.vue'
+import AmTabsItem from './tabs/tabsItem.vue'
+import AmTabsBody from './tabs/tabsBody.vue'
+import AmTabsPane from './tabs/tabsPane.vue'
+import AmToast from './toast/index.js'
+import AmMessage from './message/index.js'
+import AmUploader from './uploader/uploader.vue'
+import AmIcon from './icon.vue'
+import AmCollapseTransition from './collapseTransition.vue'
+import AmClickOutside from './clickOutside.js'
+import Validator from './validate.js'
 
 import '../style/index.scss'
 
 const components = [
-  Button,
-  ButtonGroup,
-  ControllCount,
-  Cascader,
-  CascaderItem,
-  Collapse,
-  CollapseItem,
-  DatePicker,
-  Col,
-  Row,
-  Input,
-  Select,
-  Option,
-  Checkbox,
-  Radio,
-  Table,
-  TableColumn,
-  Layout,
-  Header,
-  Aside,
-  Content,
-  Footer,
-  Nav,
-  NavItem,
-  SubNav,
-  Pager,
-  Popover,
-  Slides,
-  SlidesItem,
-  Tabs,
-  TabsHeader,
-  TabsItem,
-  TabsBody,
-  TabsPane,
-  Uploader,
-  Icon,
-  CollapseTransition
+  AmButton,
+  AmButtonGroup,
+  AmControllCount,
+  AmCascader,
+  AmCascaderItem,
+  AmCollapse,
+  AmCollapseItem,
+  AmDatePicker,
+  AmCol,
+  AmRow,
+  AmInput,
+  AmSelect,
+  AmOption,
+  AmCheckbox,
+  AmRadio,
+  AmTable,
+  AmTableColumn,
+  AmLayout,
+  AmHeader,
+  AmAside,
+  AmContent,
+  AmFooter,
+  AmNav,
+  AmNavItem,
+  AmSubNav,
+  AmPager,
+  AmPopover,
+  AmSlides,
+  AmSlidesItem,
+  AmTabs,
+  AmTabsHeader,
+  AmTabsItem,
+  AmTabsBody,
+  AmTabsPane,
+  AmUploader,
+  AmIcon,
+  AmCollapseTransition
 ]
 const install = (Vue, opts = {}) => {
-    components.forEach(component => {
-        Vue.component(component.name, component)
-    })
-    Vue.use(Toast)
+  components.forEach(component => {
+    Vue.component(component.name, component)
+  })
+  Vue.use(AmToast)
+  Vue.use(AmMessage)
 }
 export default install
 export {
-  Button,
-  ButtonGroup,
-  ControllCount,
-  Cascader,
-  CascaderItem,
-  Collapse,
-  CollapseItem,
-  DatePicker,
-  Col,
-  Row,
-  Input,
-  Checkbox,
-  Radio,
-  Select,
-  Option,
-  Table,
-  TableColumn,
-  Layout,
-  Header,
-  Aside,
-  Content,
-  Footer,
-  Nav,
-  NavItem,
-  SubNav,
-  Pager,
-  Popover,
-  Slides,
-  SlidesItem,
-  Tabs,
-  TabsHeader,
-  TabsItem,
-  TabsBody,
-  TabsPane,
-  Toast,
-  Uploader,
-  Icon,
-  ClickOutside,
-  CollapseTransition
+  AmButton,
+  AmButtonGroup,
+  AmControllCount,
+  AmCascader,
+  AmCascaderItem,
+  AmCollapse,
+  AmCollapseItem,
+  AmDatePicker,
+  AmCol,
+  AmRow,
+  AmInput,
+  AmCheckbox,
+  AmRadio,
+  AmSelect,
+  AmOption,
+  AmTable,
+  AmTableColumn,
+  AmLayout,
+  AmHeader,
+  AmAside,
+  AmContent,
+  AmFooter,
+  AmNav,
+  AmNavItem,
+  AmSubNav,
+  AmPager,
+  AmPopover,
+  AmSlides,
+  AmSlidesItem,
+  AmTabs,
+  AmTabsHeader,
+  AmTabsItem,
+  AmTabsBody,
+  AmTabsPane,
+  AmToast,
+  AmMessage,
+  AmUploader,
+  AmIcon,
+  AmClickOutside,
+  Validator,
+  AmCollapseTransition
 }
